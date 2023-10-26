03e10121e2a44145cede80a5162ecda7be2d97c0
Jaime Ilustre <jaimeilustre@yahoo.com>
2023-10-26 13:50:59 +0200

    Left and right functionality done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log("js loaded....")
+class Player {
+    constructor() {
+        // initialize properties
+        this.positionX = 50;
+        this.positionY = 0;
+        this.height = 10;
+        this.width = 20;
+
+        // dom manipulation
+        this.playerElm = document.getElementById("player");
+        this.playerElm.style.width = this.width + "vw";
+        this.playerElm.style.height = this.height + "vh";
+        this.playerElm.style.left= this.positionX + "vw";
+        this.playerElm.style.bottom = this.positionY + "vh";
+        
+    }
+    moveLeft() {
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+    moveRight() {
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+}
+
+const player = new Player();
+
+document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            player.moveLeft();
+            break;
+        case "ArrowRight":
+            player.moveRight();
+            break;
+    }
+})
